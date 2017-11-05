91209d3a28573c4ab09ea06722c60a81c79f4d08
minotauri <ahnguyen@gmail.com>
2017-11-05 11:19:17 -0800

    Coaching Bug Fixes

--- src/main/java/Simulation/League.java
+++ src/main/java/Simulation/League.java
@@ -3561,7 +3561,7 @@ public class League {
             String tmName = coachSplit[0];
             int tmPres = Integer.parseInt(coachSplit[1]);
             for (int t = 0; t < teamList.size(); ++t) {
-                if (teamList.get(t).HC.isEmpty() && (coachStarList.get(i).ratOvr - 8) >= teamList.get(t).teamPrestige && teamList.get(t).name != tmName && teamList.get(t).teamPrestige >= tmPres) {
+                if (teamList.get(t).HC.isEmpty() && (coachStarList.get(i).ratOvr + 5) >= teamList.get(t).teamPrestige && teamList.get(t).name != tmName && teamList.get(t).teamPrestige >= tmPres) {
                     teamList.get(t).HC.add(coachStarList.get(i));
                     newsStories.get(currentWeek + 1).add("Coaching Hire: " + teamList.get(t).name + ">Rising star head coach " + teamList.get(t).HC.get(0).name + " has announced his departure from " +
                      coachStarPrevTeam.get(i) + " after being selected by " + teamList.get(t).name + " as their new head coach. His previous track record has had him on the top list of many schools.");
