10df9e7726bc497abef33d7a4f5326e7bc16c912
minotauri <ahnguyen@gmail.com>
2017-11-20 15:39:40 -0800

    Long Term Game Balancing Changes

--- src/main/java/Simulation/League.java
+++ src/main/java/Simulation/League.java
@@ -2664,6 +2664,149 @@ public class League extends Rankings {
         for (int i = 0; i < teamList.size(); ++i) {
             teamList.get(i).sortPlayers();
         }
+
+        for (int i = 0; i < transferQBs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamQBs.size() < 1 || teamList.get(t).teamQBs.get(0).ratOvr < transferQBs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferQBs.get(i).region) < 3){
+                        teamList.get(t).teamQBs.add(transferQBs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>QB " + transferQBs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tQBs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferRBs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamRBs.size() < 2 || teamList.get(t).teamRBs.get(0).ratOvr < transferRBs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferRBs.get(i).region) < 3){
+                        teamList.get(t).teamRBs.add(transferRBs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>RB " + transferRBs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tRBs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferWRs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamWRs.size() < 3 || teamList.get(t).teamWRs.get(0).ratOvr < transferWRs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferWRs.get(i).region) < 3) {
+                        teamList.get(t).teamWRs.add(transferWRs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>WR " + transferWRs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tWRs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferTEs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamTEs.size() < 1 || teamList.get(t).teamTEs.get(0).ratOvr < transferTEs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferTEs.get(i).region) < 3) {
+                        teamList.get(t).teamTEs.add(transferTEs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>TE " + transferTEs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tTEs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferOLs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamOLs.size() < 5 || teamList.get(t).teamOLs.get(0).ratOvr < transferOLs.get(i).ratOvr && Math.abs(teamList.get(t).location - transferOLs.get(i).region) < 3) {
+                    teamList.get(t).teamOLs.add(transferOLs.get(i));
+                    newsStories.get(currentWeek+1).add("Transfer News>OL " + transferOLs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                            tOLs.get(i).toString() + " .");
+                    break;
+
+                }
+            }
+        }
+
+        for (int i = 0; i < transferKs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamKs.size() < 1 || teamList.get(t).teamKs.get(0).ratOvr < transferKs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferKs.get(i).region) < 3) {
+                        teamList.get(t).teamKs.add(transferKs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>K " + transferKs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tKs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferDLs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamDLs.size() < 4 || teamList.get(t).teamDLs.get(0).ratOvr < transferDLs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferDLs.get(i).region) < 3) {
+                        teamList.get(t).teamDLs.add(transferDLs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>DL " + transferDLs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tDLs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferLBs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamLBs.size() < 3 || teamList.get(t).teamLBs.get(0).ratOvr < transferLBs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferLBs.get(i).region) < 3) {
+                        teamList.get(t).teamLBs.add(transferLBs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>LB " + transferLBs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tLBs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferCBs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamCBs.size() < 3 || teamList.get(t).teamCBs.get(0).ratOvr < transferCBs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferCBs.get(i).region) < 3) {
+                        teamList.get(t).teamCBs.add(transferCBs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>CB " + transferCBs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tCBs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < transferSs.size(); ++i) {
+            rand = random.nextInt((max - min) + 1) + min;
+            for (int t = rand; t < teamList.size() - rand; ++t) {
+                if (teamList.get(t).teamSs.size() < 1 || teamList.get(t).teamSs.get(0).ratOvr < transferSs.get(i).ratOvr) {
+                    if (Math.abs(teamList.get(t).location - transferSs.get(i).region) < 3) {
+                        teamList.get(t).teamSs.add(transferSs.get(i));
+                        newsStories.get(currentWeek+1).add("Transfer News>S " + transferSs.get(i).name + " has announced his transfer to " + teamList.get(t).name + ". He was previously enrolled at " +
+                                tSs.get(i).toString() + " .");
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < teamList.size(); ++i) {
+            teamList.get(i).sortPlayers();
+        }
     }
 
 
@@ -2713,14 +2856,14 @@ public class League extends Rankings {
         //Field Upgrade Chance 1
         int luckyNumber = (int)(Math.random()*40);
         Team luckyTeam = teamList.get(luckyNumber);
-        luckyTeam.teamPrestige += (int)(Math.random()*6);
+        luckyTeam.teamPrestige += (int)(Math.random()*4);
         saveLucky = luckyTeam;
         if (luckyTeam.teamPrestige > 99) {luckyTeam.teamPrestige = 95;}
 
         //Field Upgrade Chance 2
         luckyNumber = (int)(Math.random()*40);
         Team luckyTeam2 = teamList.get(40 + luckyNumber);
-        luckyTeam2.teamPrestige += (int)(Math.random()*9);
+        luckyTeam2.teamPrestige += (int)(Math.random()*6);
         saveLucky2 = luckyTeam2;
         if (luckyTeam2.teamPrestige > 99) {luckyTeam2.teamPrestige = 95;}
 
@@ -2728,7 +2871,7 @@ public class League extends Rankings {
         luckyNumber = (int)(Math.random()*40);
         if (luckyNumber > 39) luckyNumber = 39;
         Team luckyTeam3 = teamList.get(80 + luckyNumber);
-        luckyTeam3.teamPrestige += (int)(Math.random()*12);
+        luckyTeam3.teamPrestige += (int)(Math.random()*8);
         saveLucky3 = luckyTeam3;
         if (luckyTeam3.teamPrestige > 99) {luckyTeam3.teamPrestige = 95;}
 
